aade1b2de12f132b74f9146345611b802178c7d4
TylerRosen <tylermrosen@gmail.com>
2017-05-01 21:19:35 -0400

    Rankings display when button is clicked

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* Set the width of the side navigation to 250px and the right margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "450px";
-    document.getElementById("main").style.marginLeft = "450px";
+    document.getElementById("main").style.marginRight = "450px";
+    document.getElementById("arrow").style.marginRight = "450px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+/* Set the width of the side navigation to 0 and the right margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("arrow").style.marginRight = "0";
 }
 
-$('i.fa.fa-angle-double-left ').click(function (){
+$('i.fa.fa-angle-double-left ').click(function() {
     openNav();
 })
+
+$('#display').click(function() {
+    $('#players').show();
+    $('#display').hide();
+})
+
+$('.addTeam').click(function() {
+    $('#myPlayers').append($('span').text());
+})
+
+$('#searchBox').submit(function() {
+    console.log('hello');
+});
