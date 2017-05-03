f629ec6ce65eca2c8a95e1bd4152518b994b00cc
Tyler Rosen <tylermrosen@gmail.com>
2017-05-02 20:31:43 -0400

    Created routes and orm files

--- server.js
+++ server.js
@@ -3,48 +3,23 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 var mysql = require('mysql');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-
-var port = process.env.PORT || 3000;
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
-//allow sessions
-app.use(session({ secret: 'app', cookie: { maxAge: 6 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 } }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+var routes = require('./controllers/routes.js');
+app.use('/', routes);
 
 
-var statsController = require("./controllers/statsController.js");
-
+var port = process.env.PORT || 3000;
 app.listen(port);
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "raiders76",
-    database: "fantasy_db"
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log('Connected as id: ' + connection.threadId);
-})
-
-app.get('/', function(req, res) {
-    // res.send('hi')
-    connection.query('SELECT * FROM tophundred;', function(err, data) {
-        //res.json(data);
-        res.render('index', { tophundred: data });
-    })
-})
