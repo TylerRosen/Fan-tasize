313154e7673fc5aa203baab2ff6e2765710172ef
TylerRosen <tylermrosen@gmail.com>
2017-05-03 19:34:40 -0400

    Adjusted width of sidebar

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,15 +1,15 @@
 /* Set the width of the side navigation to 250px and the right margin of the page content to 250px */
 function openNav() {
-    $("#mySidenav").width(450);
-    $("#main").css({marginRight:'450px'});
-    $("#arrow").css({marginRight:'420px'});
+    $("#mySidenav").width(350);
+    $("#main").css({ marginRight: '450px' });
+    $("#arrow").css({ marginRight: '312px' });
 }
 
 /* Set the width of the side navigation to 0 and the right margin of the page content to 0 */
 function closeNav() {
     $("#mySidenav").width(0);
-    $("#main").css({marginRight:'0'});
-    $("#arrow").css({marginRight:'-32px'});
+    $("#main").css({ marginRight: '0' });
+    $("#arrow").css({ marginRight: '-32px' });
 }
 
 $('i.fa.fa-angle-double-left ').click(function() {
