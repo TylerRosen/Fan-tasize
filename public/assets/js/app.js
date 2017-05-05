0cdcd480845e0fa38c71587fe7e7151c7d402985
TylerRosen <tylermrosen@gmail.com>
2017-05-05 15:04:52 -0400

    Adjusted heading and form styling

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,18 +1,18 @@
 /* Set the width of the side navigation to 250px and the right margin of the page content to 250px */
 function openNav() {
     $("#mySidenav").css({
-        "width": '336',
+        "width": '300',
         "border-style": "solid",
-        "border-color": "grey"
+        "border-color": "white"
     });
     $(".container").css({ marginRight: '450px' });
-    $("#main").css({ marginRight: '350px' });
-    $("#arrow").css({ marginRight: '297px' });
+    $("#main").css({ marginRight: '320px' });
+    $("#arrow").css({ marginRight: '267px' });
 }
 
 /* Set the width of the side navigation to 0 and the right margin of the page content to 0 */
 function closeNav() {
-    $("#mySidenav").width(0);
+    $("#mySidenav").css({ "width": '0', "border": "none" });
     $("#main").css({ marginRight: '0' });
     $(".container").css({ margin: '0 auto' });
     $("#arrow").css({ marginRight: '-32px' });
